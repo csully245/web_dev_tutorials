2dbabeea4c0c5db4b891ac73f9155180e1b8956a
csully245 <csully245@gmail.com>
2023-01-28 14:27:26 -0500

    Completed 'Use an External File as a Template'

---
+++ jinja_realpython/write_messages.py
@@ -0,0 +1,21 @@
+import jinja2
+
+max_score = 100
+test_name = "Python Challenge"
+students = [
+    {"name": "Sandrine", "score": 100},
+    {"name": "Gergeley", "score": 87},
+    {"name": "Frieda", "score": 92},
+    {"name": "Fritz", "score": 40},
+    {"name": "Sirius", "score": 75},
+]
+
+environment = jinja2.Environment(loader=jinja2.FileSystemLoader("templates/"))
+template = environment.get_template("message.txt")
+
+for student in students:
+    filename = f"message_{student['name'].lower()}.txt"
+    content = template.render(student, max_score=max_score, test_name=test_name)
+    with open(filename, mode="w", encoding="utf-8") as message:
+        message.write(content)
+        print(f"... wrote {filename}")
